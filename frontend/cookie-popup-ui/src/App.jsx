69a899f7e54adeb22919392facb3b80a666cb458
Bubble <emiltols2001@outlook.dk>
2023-11-21 12:28:55 +0100

    cookie popup

---
+++ frontend/cookie-popup-ui/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Popup from './lib'
+
+const App = () => {
+  return (
+    <Popup />
+  )
+}
+
+export default App
---
+++ frontend/cookie-popup-ui/src/lib/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Popup() {
+  return (
+    <div id='popup-container' className='overlay'>
+        <div className='popup'>
+            Hello there i got some cookies for you
+        </div>
+    </div>
+  )
+}
