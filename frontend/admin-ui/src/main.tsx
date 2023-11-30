1ef3d433585e13a7c43730c8d1d29a1d956c472a
Bubble <49866759+BubbleDK@users.noreply.github.com>
2023-11-30 17:49:59 +0100

    Update main.tsx

--- frontend/admin-ui/src/main.tsx
+++ frontend/admin-ui/src/main.tsx
@@ -7,9 +7,7 @@ import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MantineProvider defaultColorScheme="dark" theme={{
-      fontFamily: 'Nunito, sans-serif',
-    }}>
+    <MantineProvider defaultColorScheme="dark" theme={{fontFamily: 'Nunito, sans-serif'}}>
       <App />
     </MantineProvider>
   </React.StrictMode>,
