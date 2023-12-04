fd575bfac4f25230b66887483b68e33d3d88a450
fundish <funderchristian@gmail.com>
2023-12-04 11:50:12 +0100

    SCRUM-107-Create-Read-Cookie

--- backend/DMA-FinalProject/DMA-FinalProject.API/Program.cs
+++ backend/DMA-FinalProject/DMA-FinalProject.API/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped <IDMAFinalProjectDAO<Employee>, EmployeeDAO>();
 builder.Services.AddScoped<LoginDAO>();
+builder.Services.AddScoped<CookieDAO>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
