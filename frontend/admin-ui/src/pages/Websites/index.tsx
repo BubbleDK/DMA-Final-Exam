5ccb4dfefbe833e7d73cea7aa50fac4a4bb05346
Bubble <emiltols2001@outlook.dk>
2023-12-14 11:00:55 +0100

    websites ui done

--- frontend/admin-ui/src/pages/Websites/index.tsx
+++ frontend/admin-ui/src/pages/Websites/index.tsx
@@ -1,15 +1,71 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { useAuth } from '../../utils/Auth';
 import './index.css'
+import axios from 'axios';
+import { ActionIcon, Group, ScrollArea, Table } from '@mantine/core';
+import { IconSettings } from '@tabler/icons-react';
+
+type Website = {
+  url: string;
+  name: string;
+  companyId: number;
+}
 
 const Websites = () => {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, employeeData } = useAuth();
+  const [websites, setWebsites] = useState<Website[]>([]);
 
-  if (!isLoggedIn) return;
+  useEffect(() => {
+    if (!isLoggedIn || !employeeData) return;
+
+    const getEmployeeWebsitesData = async (companyId: number) => {
+      try {
+        const url = `https://localhost:7163/domains/${companyId}`;
+
+        const response = await axios.get(url);
+
+        setWebsites(response.data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    }
+
+    getEmployeeWebsitesData(employeeData.companyId);
+  }, [isLoggedIn, employeeData]);
+
+  const websitesRow = websites.map((website) => (
+    <Table.Tr key={website.name}>
+      <Table.Td>{website.url}</Table.Td>
+      <Table.Td>12/01/2024</Table.Td>
+      <Table.Td>24</Table.Td>
+      <Table.Td>
+        <Group>
+          <ActionIcon variant="light" color="gray" aria-label="Settings">
+            <IconSettings size="1rem" stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+  if (!isLoggedIn) return null;
 
   return (
     <div className="websites-container">
-      Websites
+      <h2>Domæneoversigt</h2>
+      <ScrollArea h={735}>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Domæne URL</Table.Th>
+              <Table.Th>Sidste scan</Table.Th>
+              <Table.Th>Cookies</Table.Th>
+              <Table.Th></Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{websitesRow}</Table.Tbody>
+        </Table>
+      </ScrollArea>
     </div>
   )
 }
