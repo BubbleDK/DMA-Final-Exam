4b0f990ffa25127e83d4198a8b78cb956d349e00
Jakob Kjeldsteen <jkjeldsteen@gmail.com>
2023-12-18 11:09:29 +0100

    Documentation for Employee, Login and Cookie API tests

--- backend/DMA-FinalProject/Tests/EmployeeAPITests.cs
+++ backend/DMA-FinalProject/Tests/EmployeeAPITests.cs
@@ -7,61 +7,91 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Tests
 {
+    /// <summary>
+    /// This test class contains xUnit tests for the EmployeeController, covering various CRUD operations.
+    /// </summary>
     public class EmployeeAPITests
     {
+        // Create an instance of EmployeeController for testing
         EmployeeController employeeController = new EmployeeController(new EmployeeDAO());
 
+        /// <summary>
+        /// Verifies the ability of the EmployeeController to add a new employee.
+        /// </summary>
+        /// <remarks>
+        /// This test covers the following steps:
+        /// - Arrange: Create an instance of EmployeeDTO with test data.
+        /// - Act: Add the test employee using the Add action.
+        /// - Assert: Ensure that the action returns a non-null result, the HTTP status code is 200 OK,
+        ///           and the extracted result value is true indicating successful addition.
+        /// - Cleanup: Remove the test employee from the database to maintain a clean state.
+        /// </remarks>
         [Fact]
         public void TestEmployeeAdd()
         {
-            //Arrange
+            // Arrange
             EmployeeDTO employee = new EmployeeDTO("Bob", "88888888", "Bob@test.dk", "password123", 1);
 
-            //Act
+            // Act
             ActionResult<bool> actionResult = employeeController.Add(employee);
 
-            //Assert
+            // Assert
             Assert.NotNull(actionResult);
-
-            // Check if the HTTP status code is 200 OK
             Assert.IsType<OkObjectResult>(actionResult.Result);
-
-            // Extract the value from OkObjectResult and check if it's true
             bool resultValue = (bool)((OkObjectResult)actionResult.Result).Value;
             Assert.True(resultValue);
 
-            //Cleanup after
+            // Cleanup
             employeeController.Delete("Bob@test.dk");
         }
 
+        /// <summary>
+        /// Verifies the ability of the EmployeeController to retrieve an employee by email.
+        /// </summary>
+        /// <remarks>
+        /// This test covers the following steps:
+        /// - Arrange: Insert a test employee into the database.
+        /// - Act: Retrieve the employee by email using the Get action.
+        /// - Assert: Ensure that the action returns a non-null result, and the retrieved employee's email matches the input email.
+        /// - Cleanup: Remove the test employee from the database to maintain a clean state.
+        /// </remarks>
         [Fact]
         public void TestEmployeeGet()
         {
-            //Arrange
-            //Inserting a user to get
+            // Arrange
+            // Inserting a user to get
             EmployeeDTO insertedEmployee = new EmployeeDTO("Bob", "88888888", "Bob@test.dk", "password123", 1);
             employeeController.Add(insertedEmployee);
 
-            //Act
+            // Act
             ActionResult<EmployeeDTO> actionResult = employeeController.Get("Bob@test.dk");
 
-            //Assert
+            // Assert
             Assert.NotNull(actionResult);
-
-            //Check if the returned employee matches the input email
             OkObjectResult okResult = (OkObjectResult)actionResult.Result;
             EmployeeDTO employee = (EmployeeDTO)okResult.Value;
             Assert.Equal("Bob@test.dk", employee.Email);
 
-            //Cleanup after
+            // Cleanup
             employeeController.Delete("Bob@test.dk");
         }
 
+        /// <summary>
+        /// Verifies the ability of the EmployeeController to retrieve all employees.
+        /// </summary>
+        /// <remarks>
+        /// This test covers the following steps:
+        /// - Arrange: Insert some test data, including multiple employees, into the database.
+        /// - Act: Retrieve all employees using the GetAll action.
+        /// - Assert: Ensure that the action returns a non-null result, the HTTP status code is 200 OK,
+        ///           and the result value is of type List<EmployeeDTO>.
+        /// - Cleanup: Remove the test employees from the database to maintain a clean state.
+        /// </remarks>
         [Fact]
         public void TestEmployeeGetAll()
         {
-            //Arrange
-            //Inserting some test data
+            // Arrange
+            // Inserting some test data
             EmployeeDTO employee = new EmployeeDTO("Bob", "88888888", "Bob@test.dk", "password123", 1);
             employeeController.Add(employee);
             EmployeeDTO employee2 = new EmployeeDTO("Bobby", "77777777", "Bobby@test.dk", "password456", 1);
@@ -69,38 +99,47 @@ namespace Tests
             EmployeeDTO employee3 = new EmployeeDTO("Bobber", "99999999", "Bobber@test.dk", "password789", 1);
             employeeController.Add(employee3);
 
-
-            //Act
+            // Act
             ActionResult<IEnumerable<EmployeeDTO>> actionResult = employeeController.GetAll();
 
-            //Assert
+            // Assert
             Assert.NotNull(actionResult);
-
-            //Check if the HTTP status code is 200 OK
             Assert.IsType<OkObjectResult>(actionResult.Result);
             OkObjectResult result = (OkObjectResult)actionResult.Result;
             Assert.IsType<List<EmployeeDTO>>(((IEnumerable<EmployeeDTO>)result.Value).ToList());
 
-            //Cleanup after
+            // Cleanup
             employeeController.Delete("Bob@test.dk");
             employeeController.Delete("Bobby@test.dk");
             employeeController.Delete("Bobber@test.dk");
         }
 
+
+        /// <summary>
+        /// Verifies the ability of the EmployeeController to update an existing employee.
+        /// </summary>
+        /// <remarks>
+        /// This test covers the following steps:
+        /// - Arrange: Insert a test employee into the database.
+        /// - Act: Update the employee's name and phone number using the Update action.
+        /// - Assert: Ensure that the action returns a non-null result, the HTTP status code is 200 OK,
+        ///           and the extracted result value is true indicating successful update.
+        /// - Cleanup: Remove the test employee from the database to maintain a clean state.
+        /// </remarks>
         [Fact]
         public void TestEmployeeUpdate()
         {
-            //Arrange
-            //Insert an employee to be updated
+            // Arrange
+            // Insert an employee to be updated
             EmployeeDTO employee = new EmployeeDTO("Bob", "88888888", "Bob@test.dk", "password123", 1);
             employeeController.Add(employee);
 
-            //Act
+            // Act
             employee.Name = "Bobster";
             employee.Phone = "12345678";
             ActionResult<bool> actionResult = employeeController.Update(employee);
 
-            //Assert
+            // Assert
             Assert.NotNull(actionResult);
 
             // Check if the HTTP status code is 200 OK
@@ -110,21 +149,31 @@ namespace Tests
             bool resultValue = (bool)((OkObjectResult)actionResult.Result).Value;
             Assert.True(resultValue);
 
-            //Cleanup after
+            // Cleanup
             employeeController.Delete("Bob@test.dk");
         }
 
+
+        /// <summary>
+        /// Verifies the ability of the EmployeeController to delete an existing employee.
+        /// </summary>
+        /// <remarks>
+        /// This test covers the following steps:
+        /// - Arrange: Insert a test employee into the database.
+        /// - Act: Delete the test employee using the Delete action.
+        /// - Assert: Ensure that the action returns a non-null result, and the HTTP status code is 200 OK.
+        /// </remarks>
         [Fact]
         public void TestEmployeeDelete()
         {
-            //Arrange
+            // Arrange
             EmployeeDTO employee = new EmployeeDTO("Bob", "88888888", "Bob@test.dk", "password123", 1);
             employeeController.Add(employee);
 
-            //Act
+            // Act
             ActionResult<bool> actionResult = employeeController.Delete("Bob@test.dk");
 
-            //Assert
+            // Assert
             Assert.NotNull(actionResult);
 
             // Check if the HTTP status code is 200 OK
--- backend/DMA-FinalProject/Tests/LoginAPITests.cs
+++ backend/DMA-FinalProject/Tests/LoginAPITests.cs
@@ -12,18 +12,32 @@ using System.Threading.Tasks;
 
 namespace Tests
 {
+    /// <summary>
+    /// This test class contains xUnit tests for the LoginController, specifically focusing on the login functionality.
+    /// </summary>
     public class LoginAPITests
     {
+        // Configuration instance for test setup
         private IConfiguration configuration;
-        private LoginController loginController;
-        EmployeeController employeeController = new EmployeeController(new EmployeeDAO());
 
+        // LoginController instance for testing
+        private LoginController loginController;
+
+        // EmployeeController instance for additional setup
+        private EmployeeController employeeController = new EmployeeController(new EmployeeDAO());
+
+        /// <summary>
+        /// Initializes a new instance of the LoginAPITests class.
+        /// </summary>
         public LoginAPITests()
         {
             InitializeConfiguration();
             InitializeLoginController();
         }
 
+        /// <summary>
+        /// Initializes the configuration for the test class.
+        /// </summary>
         private void InitializeConfiguration()
         {
             configuration = new ConfigurationBuilder()
@@ -32,24 +46,38 @@ namespace Tests
                     .Build();
         }
 
+        /// <summary>
+        /// Initializes the LoginController instance for testing.
+        /// </summary>
         private void InitializeLoginController()
         {
             LoginDAO loginDAO = new LoginDAO();
             loginController = new LoginController(loginDAO, configuration);
         }
 
+        /// <summary>
+        /// Verifies the ability of the LoginController to handle the login process.
+        /// </summary>
+        /// <remarks>
+        /// This test covers the following steps:
+        /// - Arrange: Insert a test employee into the database and create a LoginDTO with login credentials.
+        /// - Act: Call the Login method of LoginController with the LoginDTO.
+        /// - Assert: Ensure that the login action returns a non-null result, and the returned employee's email matches the input email.
+        ///           Additionally, check the token validity using the CheckToken method.
+        /// - Cleanup: Remove the test employee from the database to maintain a clean state.
+        /// </remarks>
         [Fact]
         public void TestLoginControllerLoginMethod()
         {
-            //Arrange
+            // Arrange
             EmployeeDTO insertedEmployee = new EmployeeDTO("Bob", "88888888", "Bob@test.dk", "test123", 1);
             employeeController.Add(insertedEmployee);
             LoginDTO loginDTO = new LoginDTO("Bob@test.dk", "test123");
 
-            //Act
+            // Act
             ActionResult<EmployeeDTO> actionResult = loginController.Login(loginDTO);
 
-            //Assert
+            // Assert
             Assert.NotNull(actionResult);
             OkObjectResult okResult = (OkObjectResult)actionResult.Result;
             EmployeeDTO employee = (EmployeeDTO)okResult.Value;
@@ -59,8 +87,9 @@ namespace Tests
             Assert.NotNull(result);
             Assert.Equal(true, result.Value);
 
-            //Cleanup
+            // Cleanup
             employeeController.Delete("Bob@test.dk");
         }
     }
+
 }
