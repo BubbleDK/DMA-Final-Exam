294e10625993fcacab41841f70f5c4d03df15416
Bubble <emiltols2001@outlook.dk>
2024-01-03 11:16:56 +0100

    Update index.jsx

--- frontend/cookie-popup-ui/src/lib/index.jsx
+++ frontend/cookie-popup-ui/src/lib/index.jsx
@@ -9,13 +9,13 @@ export default function Popup() {
 	const [statistics, setStatistics] = useState(true);
 	const [marketing, setMarketing] = useState(true);
 
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJsYXJzQGxhcnMuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9zZXJpYWxudW1iZXIiOiJjYWZlNGE4OS0wMjE4LTRiNjItODA5MC00NjA2MmI2ZmFmM2EiLCJleHAiOjIwMTg1OTYwNTR9.vMlBM98uD0gi8VKRRTgOK7ePQ4A5eQaRerGJjAYTp9I",
-    },
-  };
+	const config = {
+		headers: {
+			"Content-Type": "application/json",
+			Authorization:
+				"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJsYXJzQGxhcnMuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9zZXJpYWxudW1iZXIiOiJjYWZlNGE4OS0wMjE4LTRiNjItODA5MC00NjA2MmI2ZmFmM2EiLCJleHAiOjIwMTg1OTYwNTR9.vMlBM98uD0gi8VKRRTgOK7ePQ4A5eQaRerGJjAYTp9I",
+		},
+	};
 
 	useEffect(() => {
 		let uid = localStorage.getItem("userId");
@@ -23,22 +23,22 @@ export default function Popup() {
 			uid = generateUserId();
 			localStorage.setItem("userId", uid);
 
-      axios
-      .post(
-        "https://localhost:7163/api/Users",
-        {
-          "browserId": uid,
-        },
-        config
-      )
-      .then((response) => {
-        console.log("User created:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error sending consent:", error);
-      });
+			axios
+				.post(
+					"https://localhost:7163/api/Users",
+					{
+						"browserId": uid,
+					},
+					config
+				)
+				.then((response) => {
+					console.log("User created:", response.data);
+				})
+				.catch((error) => {
+					console.error("Error sending consent:", error);
+				});
 
-      setShowPopup(true);
+			setShowPopup(true);
 		}
 		setUserId(uid);
 	}, []);
